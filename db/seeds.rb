c1e9570ab1de09409105c09772bcafb9767fc54c
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-18 14:33:14 -0700

    movies seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+movie = CSV.read('movies.csv')
+
+i = 1
+while i < movie.length
+  Movie.create(
+    title: movie[i][0],
+    year: movie[i][1]
+  )
+  i += 1
+end
